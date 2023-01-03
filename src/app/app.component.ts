05ea620512484c206004e769dfa64bf86450fddb
haiderzia105 <haiderzia105>
2023-01-03 16:18:06 +0500

    update 4.17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,18 @@ export class AppComponent {
   count = 0;
   name = "Haider";
   disable = true;
+  able = false;
+  kite='yellow';
+  smile='orange';
+  forever = ['num 1','num 2','num 3','num 4','num 5'];
+  foreverDetails = [
+    {
+      name:'1',order:'1',group:'one' },
+     { name:'2',order:'2',group:'two'},
+     {name:'3',order:'3',group:'three'},
+     { name:'4',order:'4',group:'four'},
+     { name:'5',order:'5',group:'five'},
+  ]
   data = "The University of Faisalabad";
   dataprint(){
     return "TUF";
